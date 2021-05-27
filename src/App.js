40278a51add1d593b60ffe56d9f5f1b8c7a2dafc
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2021-05-28 07:56:24 +1000

    add BigNumber for formatting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+//todo: add loading button, add chainlink api to get USD prices
+
 import React, { Component } from "react";
 // import AuctionContract from "./contracts/Auction.json";
 import AuctionContract from "./contracts/SimpleAuction.json" ;
 import getWeb3 from "./getWeb3";
 import config from "./config/config.json"
-
+import BigNumber from "bignumber.js";
 import "./App.css";
 
 class App extends Component {
@@ -37,14 +39,14 @@ class App extends Component {
       const id = await instance.methods.getAuctionId().call();
       const highestBidder = await instance.methods.getHighestBidder().call();
       const highestBid = await instance.methods.getHighestBid().call();
-      const userBalance = await instance.methods.getUserBalance().call({from: accounts[0]});
+      // const userBalance = await instance.methods.getUserBalance().call({from: accounts[0]});
 
       console.log("accounts: " + accounts);
       console.log("auction owner: " + owner);
       console.log("auction id: " + id);
       console.log("highest bidder: " + highestBidder);
       console.log("highest bid: " + highestBid);
-      console.log("user balance: " + userBalance);
+      // console.log("user balance: " + userBalance);
 
       this.setState({
         web3: web3,
@@ -53,8 +55,9 @@ class App extends Component {
         auctionOwner: owner,
         auctionId: id,
         highestBid: web3.utils.fromWei(highestBid, 'ether'),
-        highestBidder: highestBidder,
-        userBalance: web3.utils.fromWei(userBalance, 'ether')});
+        highestBidder: highestBidder
+        // userBalance: web3.utils.fromWei(userBalance, 'ether')
+      });
 
       console.log(this.state);
     } catch (error) {
@@ -75,16 +78,31 @@ class App extends Component {
 
     await contract.methods.bid().send({from: accounts[0], value: bidValue});
     const highestBidder = await contract.methods.getHighestBidder().call();
-    const highestBid = await contract.methods.getHighestBid().call();
-    const userBalance = await contract.methods.getUserBalance().call({from: accounts[0]});
-    console.log("highest bidder: " + highestBidder);
-    console.log("highest bid: " + web3.utils.fromWei(highestBid, 'ether'));
-    console.log("user balance: " + userBalance);
+    const highestBid = BigNumber(await contract.methods.getHighestBid().call());
+    // const userBalance = await contract.methods.getUserBalance().call({from: accounts[0]});
+    
+    // contract.once('HighestBidIncreased', {
+    //   filter: {user: accounts[0]},
+    //   fromBlock: "latest"
+    // }, function(error, event){
+    //     if (error) {console.log(error)}
+    //     else {
+    //       console.log(event)
+    //       console.log(event.returnValues)
+    //       var eventDecoded = web3.utils.hexToUtf8(event.returnValues);
+    //       console.log(eventDecoded)
+    //     }
+    // });
+    
+    console.log("highest bidder: " + highestBidder.toString());
+    console.log("highest bid: " + web3.utils.fromWei(highestBid.toString(), 'ether'));
+    // console.log("user balance: " + userBalance);
 
     this.setState({
-      highestBid: web3.utils.fromWei(highestBid, 'ether'),
-      highestBidder: highestBidder,
-      userBalance: web3.utils.fromWei(userBalance, 'ether')});
+      highestBid: web3.utils.fromWei(highestBid.toString(), 'ether'),
+      highestBidder: highestBidder.toString(),
+      // userBalance: web3.utils.fromWei(userBalance, 'ether')
+    });
   };
 
   withdraw = async () => {
@@ -94,15 +112,16 @@ class App extends Component {
     await contract.methods.withdraw().send({from: accounts[0]});
     const highestBidder = await contract.methods.getHighestBidder().call();
     const highestBid = await contract.methods.getHighestBid().call();
-    const userBalance = await contract.methods.getUserBalance().call({from: accounts[0]});
+    // const userBalance = await contract.methods.getUserBalance().call({from: accounts[0]});
     console.log("highest bidder :" + highestBidder);
     console.log("highest bid :" + highestBid);
-    console.log("user balance: " + userBalance);
+    // console.log("user balance: " + userBalance);
 
     this.setState({
       highestBid: web3.utils.fromWei(highestBid, 'ether'),
       highestBidder: highestBidder,
-      userBalance: web3.utils.fromWei(userBalance, 'ether')});
+      // userBalance: web3.utils.fromWei(userBalance, 'ether')
+    });
   }
 
   inputHandler = (event) => {
@@ -115,13 +134,14 @@ class App extends Component {
     }
     return (
       <div className="Auction App">
-        <h1>Good to Go!</h1>
+        <h1>Auction DApp with Chainlink</h1>
+        <img src="https://cdn.coil.com/cdn-cgi/image/format=auto,fit=scale-down,w=1920/images/Gji3k7UvQP-lRlaqnaSOOg.jpg" alt="Parks and Rec DApp img"></img>
         <div>Auction owner address: <strong>{this.state.auctionOwner}</strong>
             <br/> Highest bidder: <strong>{(this.state.highestBidder == this.state.accounts[0]) ? "You are the highest bidder" : this.state.highestBidder}</strong>
             <br/> Highest bid: <strong>{this.state.highestBid} ether</strong>
         </div>
         <div>Auction ID: <strong>{this.state.auctionId}</strong>
-          <br/>Your total bid: <strong>{this.state.userBalance} ether</strong>
+          {/* <br/>Your total bid: <strong>{this.state.userBalance} ether</strong> */}
         </div>
         <p>Enter your bid value in ether</p>
         <input
