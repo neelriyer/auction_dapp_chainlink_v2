d63b5a1ef1bd189cb79dabaca1ddb0aa161d5e43
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2021-05-29 15:09:32 +1000

    init commit

--- backend/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var auction = artifacts.require("SimpleAuction");
-var price = artifacts.require("PriceConsumerV3");
-var limit = 1000000000;
-
-module.exports = function(deployer) {
-    deployer.deploy(auction, limit, "0xBea10eB56a156516998Ec233a1B48C9a7e30A22d");
-    deployer.deploy(price);
-}
---
+++ precache-manifest.62afc80bc44e666d71fbb2d5dd4b5956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ca952bdc3e8c2f8d7bcb9b5ffc3fae",
+    "url": "/auction-app/index.html"
+  },
+  {
+    "revision": "38cddcd10c4d0251fdac",
+    "url": "/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f6bfb8b83009aafc61d",
+    "url": "/auction-app/static/js/2.87d11507.chunk.js"
+  },
+  {
+    "revision": "38cddcd10c4d0251fdac",
+    "url": "/auction-app/static/js/main.8785a0d0.chunk.js"
+  },
+  {
+    "revision": "f3bf5b93f7e7136135cd",
+    "url": "/auction-app/static/js/runtime-main.ad8c3a94.js"
+  }
+]);
